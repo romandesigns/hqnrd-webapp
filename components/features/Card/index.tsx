23fb4c14ea1518477c8c6b396b3114e341abd016
Román F <wavystyledev@gmail.com>
2025-09-08 02:53:52 +0000

    clean pu

--- components/features/Card/index.tsx
+++ components/features/Card/index.tsx
@@ -1,9 +1,4 @@
-import { IconAlertTriangle } from '@tabler/icons-react'
-
-const Header = () => <div className="">Header</div>;
-const Footer = () => <div className="">Footer</div>;
-
-export function CardComponent({
+export function Card({
   Header,
   Footer,
   children,
@@ -16,11 +11,11 @@ export function CardComponent({
     // Card Container
     <div className="bg-[var(--card-container)] rounded-md w-full max-w-md border border-[var(--card-border)] p-0.5">
       {/* Card Header */}
-      {/* {Header && (
+      {Header && (
         <div className="p-2">
           <Header />
         </div>
-      )} */}
+      )}
       {/* Card Body */}
       {children && (
         <div className="rounded-sm bg-[var(--card-body)] p-0.5 shadow-md shadow-black/10 dark:shadow-black">
@@ -36,19 +31,5 @@ export function CardComponent({
         </div>
       )}
     </div>
-  );
-}
-
-export function Card() {
-  return (
-    // Card Container
-    <CardComponent Header={Header} Footer={Footer}>
-      <div className='p-8'>
-        <h2 className="text-lg font-bold">Delete development user</h2>
-        <p className="text-xs text-muted-foreground">Are you sure you want to delete this user?</p>
-        <div className="h-0.5 my-4 w-full bg-border"/>
-        <p className="text-xs text-destructive flex items-center gap-2"><IconAlertTriangle/> This action is permanent and cannot be undone</p>
-      </div>
-    </CardComponent>
   );
 }
