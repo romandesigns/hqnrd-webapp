2c7857929762096afecaafec08994b92519575c6
Román F <wavystyledev@gmail.com>
2025-09-08 02:55:54 +0000

    clean up

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -6,8 +6,7 @@ import { Card } from "@/components/features";
 export default function PlaygroundPage() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h2 className="uppercase font-black">Card Component</h2>
-      <Card />
+      <h2 className="uppercase font-black">Playground</h2>
     </div>
   );
 }
