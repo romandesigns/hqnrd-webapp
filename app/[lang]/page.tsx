ca82fd8f62ece1f8374ac55ef43aa150b884ec46
Román F <wavystyledev@gmail.com>
2025-09-22 03:42:43 +0000

    clean up

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -12,12 +12,20 @@ export default async function Home({
   return (
     <Container lang={lang} showNavBar showFooter>
       <header className="py-54 font-sans">
-        <p className="py-4 text-sm text-center text-muted-foreground">En el corazón del Cibao — <b className="font-bold">Salcedo, Provincia Hermanas Mirabal</b></p>
-        <h2 className="uppercase font-black text-4xl py-2 max-w-2xl text-center leading-snug mx-auto">Serenidad, comodidad e higiene — todo en un mismo lugar</h2>
-         <p className="py-4 text-sm text-center max-w-lg mx-auto text-muted-foreground italic">De viaje o de paseo, nuestras unidades cuentan con todo lo necesario para ofrecerte una experiencia única.</p>
+        <p className="py-4 text-sm text-center text-muted-foreground">
+          En el corazón del Cibao —{" "}
+          <b className="font-bold">Salcedo, Provincia Hermanas Mirabal</b>
+        </p>
+        <h2 className="uppercase font-black text-4xl py-2 max-w-2xl text-center leading-snug mx-auto">
+          Serenidad, comodidad e higiene — todo en un mismo lugar
+        </h2>
+        <p className="py-4 text-sm text-center max-w-lg mx-auto text-muted-foreground italic">
+          De viaje o de paseo, nuestras unidades cuentan con todo lo necesario
+          para ofrecerte una experiencia única.
+        </p>
         <div className="flex justify-center space-x-4 mt-4">
           <Button>Accede a tu cuenta</Button>
-          <Button variant={'secondary'}>Ver habitaciones</Button>
+          <Button variant={"secondary"}>Ver habitaciones</Button>
         </div>
       </header>
     </Container>
