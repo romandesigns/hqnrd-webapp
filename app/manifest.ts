4b944e48b0038fcc2ab8afc581578bfcff05448c
Román F <wavystyledev@gmail.com>
2025-09-22 02:45:21 +0000

    housekeeping

--- app/manifest.ts
+++ app/manifest.ts
@@ -23,13 +23,11 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/assets/brand/icons/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable",
       },
       {
         src: "/assets/brand/icons/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable",
       },
       {
         src: "/assets/brand/icons/favicon-16x16.png",
