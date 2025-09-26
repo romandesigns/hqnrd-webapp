f69e5c3972f960440e29bbfe28bd7799848e1908
Román F <wavystyledev@gmail.com>
2025-09-26 19:28:27 +0000

    updated google rating

--- components/(site)/page/Home/Header/Copy.tsx
+++ components/(site)/page/Home/Header/Copy.tsx
@@ -3,28 +3,27 @@ import LottiePlayer from "@/components/features/Lottie";
 import { Button } from "@/components/ui/button";
 import { Locale } from "@/i18n-config";
 import { heatBurts, googleSquare } from "@/public/assets/animated";
-import { Highlighter } from "@/components/ui/highlighter";
 import { NumberTicker } from "@/components/ui/number-ticker";
 
 export function Copy({ lang }: { lang: Locale }) {
   return (
-    <div className="text-center lg:text-left flex flex-col space-y-3 lg:items-start justify-center items-center">
-      <div className="flex items-center justify-center lg:justify-start  lg:-ml-4 p-1">
+    <div className="pt-8 text-center lg:text-left flex flex-col space-y-3 lg:items-start justify-center items-center px-3">
+      <div className="flex items-center justify-center lg:justify-start lg:-ml-4 p-1">
         <div className="flex items-end justify-start">
-          <div className="-translate-y-2">
+          <div className="translate-y-2">
             <LottiePlayer
               item={googleSquare}
               loop
               autoplay
-              className="w-14 h-14"
+              className="w-10 h-10"
             />
           </div>
           <div>
-            <div>
+            <div className="flex items-center justify-center gap-2">
               <NumberTicker
                 value={4.5}
                 decimalPlaces={2}
-                className="text-4xl font-bold tracking-tighter whitespace-pre-wrap text-black dark:text-white"
+                className="text-sm font-bold tracking-tighter whitespace-pre-wrap text-black dark:text-white"
               />
               <StarsRating rating={4.5} />
             </div>
@@ -45,17 +44,18 @@ export function Copy({ lang }: { lang: Locale }) {
         <b className="font-bold">Salcedo, Provincia Hermanas Mirabal</b>
       </div>
       <h2 className="uppercase font-black text-2xl sm:text-4xl text-center lg:text-left leading-snug">
-        Serenidad, comodidad e higiene <br /> — todo en un mismo lugar
+        Serenidad, comodidad e higiene <br className="hidden lg:inline-block" />{" "}
+        — todo en un mismo lugar
       </h2>
       <p className="text-[.7rem] sm:text-sm lg:text-sm text-muted-foreground italic lg:max-w-lg lg:mr-auto">
         De viaje o de paseo, nuestras unidades cuentan con todo lo necesario
         para ofrecerte una experiencia única.
       </p>
       <div className="flex justify-center space-x-4 mt-4 lg:mr-auto">
-        <Button size={"lg"} variant="primary">
+        <Button className="h-10" variant="primary">
           Accede a tu cuenta
         </Button>
-        <Button variant="bordered" size={"lg"}>
+        <Button variant="bordered" className="h-10">
           Ver habitaciones
         </Button>
       </div>
